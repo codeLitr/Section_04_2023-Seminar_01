9ee0fa85c7e3daf1e7c84740e8efa780365d5840
SMV <kraft088@gmail.com>
2023-02-27 20:52:28 +1100

    Add Task04

---
+++ Seminar_01/Task_04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает трехзначное
+// число и на выходе показывает последнюю цифру этого числа.
+
+// int tripleDigitNumb = Convert.ToInt32(Console.ReadLine());
+// int lastDigit = tripleDigitNumb % 10;
+
+// System.Console.WriteLine(lastDigit);
+
+// ВАРИАНТ Решения, чтобы вывело второе число:
+
+// int tripleDigitNumb = Convert.ToInt32(Console.ReadLine());
+// int delFirstNumb = tripleDigitNumb % 100;
+// int secondNumb = delFirstNumb / 10;
+
+// System.Console.WriteLine(secondNumb);
+
+// ВАРИАНТ Решения, чтобы вывело первое число:
+int tripleDigitNumb = Convert.ToInt32(Console.ReadLine());
+int firstNumb = tripleDigitNumb / 100;
+
+System.Console.WriteLine(firstNumb);
